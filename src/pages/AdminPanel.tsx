7a89a51872cb8b549e60de759126c5a0483304d7
AlbaniCoding <albani.kalamhaq2016@gmail.com>
2026-05-15 17:54:43 +0700

    modified:   src/pages/AdminPanel.tsx 	modified:   src/pages/HomePage.tsx 	modified:   src/pages/ProductsPage.tsx

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
 function AdminPanel() {
-    const [productList, setProuductList] = useState([]);
+    // const [productList, setProuductList] = useState([]);
 
-    useEffect(() => {
-        let arr = [] as any;
-        axios.get("https://fakestoreapi.com/products")
-            .then(res => {
-                for(let i = 0; i < res.data.length; i++){
-                    arr.push(res.data[i]);
+    // useEffect(() => {
+    //     let arr = [] as any;
+    //     axios.get("https://fakestoreapi.com/products")
+    //         .then(res => {
+    //             for(let i = 0; i < res.data.length; i++){
+    //                 arr.push(res.data[i]);
                     
-                }
-                console.log(productList);
-                setProuductList(arr);
-            })
-            .catch(err => {console.error(err);});
+    //             }
+    //             console.log(productList);
+    //             setProuductList(arr);
+    //         })
+    //         .catch(err => {console.error(err);});
         
         
-     }, []);
+    //  }, []);
     return (
         <>
             
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,11 +1,11 @@
 import Navbar from "../components/Navbar";
-import { useState } from "react";
-import { register } from '../scripts/user';
-import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import { register } from '../scripts/user';
+// import { useNavigate } from "react-router-dom";
 function ProductsPage() {
-    const [user, userState] = useState("");
-    const [pass, passState] = useState("");
-    const navigate = useNavigate();
+    // const [user, userState] = useState("");
+    // const [pass, passState] = useState("");
+    // const navigate = useNavigate();
 
     return (
         <>
